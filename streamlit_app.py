70de62bc2ea952ebc00fa675963e832e75485582
Salvader Ron Nathaniel <78250990+salvaderron@users.noreply.github.com>
2022-11-27 19:09:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask
+import flask
 
 # Flask constructor takes the name of
 # current module (__name__) as argument.

750aa8bf7811ce93540a947bcb8ec034a4f87497
Salvader Ron Nathaniel <78250990+salvaderron@users.noreply.github.com>
2022-11-27 18:47:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,22 @@
-# write-html-2-windows.py
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask
 
-import webbrowser
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+app = Flask(__name__)
 
-f = open('helloworld.html','w')
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+	return 'Hello World'
 
-message = """<html>
-<head></head>
-<body><p>Hello World!</p></body>
-</html>"""
+# main driver function
+if __name__ == '__main__':
 
-f.write(message)
-f.close()
-
-webbrowser.open_new_tab('helloworld.html')
+	# run() method of Flask class runs the application
+	# on the local development server.
+	app.run()
